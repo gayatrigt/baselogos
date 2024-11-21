76793692d945c6d2a9d73ba72d0ada6e44d9a942
Utkarsh Bhimte <workwithutkarsh@gmail.com>
2024-11-21 19:23:01 +0530

    refactor generateMetadata function to improve metadata handling for NFTs

--- src/app/share/[nftId]/page.tsx
+++ src/app/share/[nftId]/page.tsx
@@ -1,6 +1,5 @@
 'use server';
 
-import Head from 'next/head';
 import { createPublicClient, http } from 'viem'
 import { base } from 'viem/chains'
 
@@ -20,56 +19,56 @@ export interface BaseLogoToken {
   image: string
 }
 
-// export async function generateMetadata({ params }: { params: { nftId: string } }): Promise<Metadata> {
-//   const tokenUri = await client.readContract({
-//     address: process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS as `0x${string}`,
-//     abi: nftContractAbi,
-//     functionName: "tokenURI",
-//     args: [BigInt(params.nftId)]
-//   })
+export async function generateMetadata({ params }: { params: { nftId: string } }): Promise<Metadata> {
+  const tokenUri = await client.readContract({
+    address: process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS as `0x${string}`,
+    abi: nftContractAbi,
+    functionName: "tokenURI",
+    args: [BigInt(params.nftId)]
+  })
 
-//   const data: BaseLogoToken = await fetch(tokenUri).then(a => a.json())
-//   const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'
+  const data: BaseLogoToken = await fetch(tokenUri).then(a => a.json())
+  const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'
 
-//   // Regular OG image URL
-//   const ogUrl = `${baseUrl}/api/og/token=${params.nftId}`
-//   // Frame-specific URL with fc=true
-//   const frameUrl = `${baseUrl}/api/og/token=${params.nftId}&fc=true`
+  // Regular OG image URL
+  const ogUrl = `${baseUrl}/api/og?token=${params.nftId}`
+  // Frame-specific URL with fc=true
+  const frameUrl = `${baseUrl}/api/og/token=${params.nftId}&fc=true`
 
-//   return {
-//     title: `${data.name} - Base Logo NFT`,
-//     description: data.description,
-//     openGraph: {
-//       title: `${data.name} - Base Logo NFT`,
-//       description: data.description,
-//       images: [
-//         {
-//           url: ogUrl,
-//           width: 1200,
-//           height: 630,
-//           alt: data.name,
-//         },
-//       ],
-//     },
-//     twitter: {
-//       card: 'summary_large_image',
-//       title: `${data.name} - Base Logo NFT`,
-//       description: data.description,
-//       images: [ogUrl],
-//     },
-//     other: {
-//       'fc:frame': 'vNext',
-//       'fc:frame:image': frameUrl,
-//       'fc:frame:image:aspect_ratio': '1:1',
-//       'fc:frame:button:1': 'Check this out',
-//       'fc:frame:button:1:action': 'link',
-//       'fc:frame:button:1:target': `https://www.baselogos.com/share/${params.nftId}`,
-//       'fc:frame:button:2': 'Mint Base Logos',
-//       'fc:frame:button:2:action': 'post',
-//       'fc:frame:button:2:target': 'https://baselogos-frame.vercel.app/frames',
-//     }
-//   }
-// }
+  return {
+    title: `${data.name} - Base Logo NFT`,
+    description: data.description,
+    openGraph: {
+      title: `${data.name} - Base Logo NFT`,
+      description: data.description,
+      images: [
+        {
+          url: ogUrl,
+          width: 1200,
+          height: 630,
+          alt: data.name,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: `${data.name} - Base Logo NFT`,
+      description: data.description,
+      images: [ogUrl],
+    },
+    other: {
+      'fc:frame': 'vNext',
+      'fc:frame:image': frameUrl,
+      'fc:frame:image:aspect_ratio': '1:1',
+      'fc:frame:button:1': 'Check this out',
+      'fc:frame:button:1:action': 'link',
+      'fc:frame:button:1:target': `https://www.baselogos.com/share/${params.nftId}`,
+      'fc:frame:button:2': 'Mint Base Logos',
+      'fc:frame:button:2:action': 'post',
+      'fc:frame:button:2:target': 'https://baselogos-frame.vercel.app/frames',
+    }
+  }
+}
 
 export default async function ShareNftPage({params}: {params: {nftId: string}}) {
 
@@ -82,12 +81,12 @@ export default async function ShareNftPage({params}: {params: {nftId: string}}) 
 
   const data: BaseLogoToken = await fetch(tokenUri).then(a => a.json())
   const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'
-  const ogUrl = `${baseUrl}/api/og/token=${params.nftId}`
-  const frameUrl = `${baseUrl}/api/og/token=${params.nftId}&fc=true`
+  const ogUrl = `${baseUrl}/api/og?token=${params.nftId}`
+  const frameUrl = `${baseUrl}/api/og?token=${params.nftId}&fc=true`
   
   return (
     <>
-       <Head>
+       {/* <Head>
         <title>{`${data.name} - Base Logo NFT`}</title>
         <meta name="description" content={data.description} />
         
@@ -111,7 +110,7 @@ export default async function ShareNftPage({params}: {params: {nftId: string}}) 
         <meta property="fc:frame:button:2" content="Mint Base Logos" />
         <meta property="fc:frame:button:2:action" content="post" />
         <meta property="fc:frame:button:2:target" content="https://baselogos-frame.vercel.app/frames" />
-      </Head>
+      </Head> */}
 
       <div className='mx-auto flex flex-col w-screen items-center justify-center px-5'>
       {/* <h1 className='md:mt-7 mt-5 text-black md:text-6xl text-2xl font-semibold'>
