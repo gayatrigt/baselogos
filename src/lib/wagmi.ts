ee7cb67d3833a13cfa7bca95c0cc5e1aacd1e945
gayatrigt <taleygayatri@gmail.com>
2024-11-21 10:43:37 +0530

    updated wallet config

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,12 +1,25 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { base, } from 'wagmi/chains';
+import { base, Chain, } from 'wagmi/chains';
 
 export const APP_NAME = 'BaseLogos';
+
+const customBase: Chain = {
+  ...base,
+  rpcUrls: {
+      ...base.rpcUrls,
+      default: {
+          http: ['https://base-mainnet.g.alchemy.com/v2/bh2cJYDWOuGInPIDaFfmCQgtWHEGUaoE'],
+      },
+      public: {
+          http: ['https://base.llamarpc.com'],
+      },
+  },
+};
 
 export const config = getDefaultConfig({
   appName: APP_NAME,
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
-  chains: [ base],
-  ssr: false,
+  chains: [customBase],
+  ssr: true,
 });
